d68c620d91aee9c47dc7f78cb895d26f5a315e4c
Victor Costa <vg47683@gmail.com>
2024-08-20 21:37:30 -0300

    Tratamento dos endereços das instruções e fluxo do ciclo de instruções

--- main.py
+++ main.py
@@ -8,22 +8,21 @@ registradores = {
     'R': None,
     'PC': 0,
     'IR': '',
-    'MAR': '',
+    'MAR': None,
     'MBR': '',
     'A': 0, # registrador extra
     'B': 0, # registrador extra
 }
 
-TAM_LINHA = 40
-ENDERECO = 6
+TAM_LINHA = 25 # tamanho total que cada linha da memoria.data possui 
+ENDERECO = 6 # tamanho para que comece a palavra de memoria
 
 def criar_memoria():
     memoria = open('memoria.data','w+b')
     for i in range(1024):
         endereco = f'0x{hex(i)[2:].zfill(3)}' # zfill complementa com zeros a apos '0x' para ate ficar com 3 casas
         memoria.write(endereco.encode())
-        memoria.write(' '.encode())
-        espaco = ((' ' * 33) + '\n').encode()
+        espaco = ((' ' * (TAM_LINHA-ENDERECO)) + '\n').encode()
         memoria.write(espaco)
 
     memoria.seek(0)
@@ -32,7 +31,7 @@ def criar_memoria():
 
 def imprime_registradores():
     print(f"AC: {registradores['AC']} | MQ: {registradores['MQ']} | C: {registradores['C']} | R: {registradores['R']} | Z: {registradores['Z']} | A: {registradores['A']} | B: {registradores['B']} ")
-    print(f"MAR: {hex(registradores['MAR'])} | MBR: {registradores['MBR']} | IR: {registradores['IR']} | PC: {registradores['PC']}")
+    print(f"MAR: {hex(registradores['MAR'])} | MBR: {registradores['MBR']} | IR: {registradores['IR']} | PC: {hex(registradores['PC'])}")
     
 def escreve_valores_memoria(arq, memoria):
     linha = arq.readline()
@@ -60,157 +59,178 @@ def escreve_instrucoes_memoria(arq, memoria):
 
         memoria.seek(0)
         num_instrucoes+=1
+    
+    memoria.seek(offset)
+    memoria.write('HALT'.encode())
 
     return num_instrucoes
 
-def verifica_opcode(parametros, memoria):
+def operacao_dados(parametros, memoria):
     if registradores['IR'] == 'LOAD':
-        inst_load(parametros, memoria)
+        load(parametros)
     elif registradores['IR'] == 'ADD':
-        inst_add(parametros, memoria)
+        add(parametros)
     elif registradores['IR'] == 'SUB':
-        inst_sub(parametros, memoria)
+        sub(parametros)
     elif registradores['IR'] == 'MOV':
-        inst_mov(parametros)
+        mov(parametros)
     elif registradores['IR'] == 'STORE':
-        inst_store(parametros, memoria)
-    elif registradores['IR'] == 'CMP':
-        inst_cmp(parametros)
+        store(parametros, memoria)
     elif registradores['IR'] == 'DIV':
-        inst_div(parametros, memoria)
+        div(parametros)
     elif registradores['IR'] == 'MULT':
-        inst_mult(parametros, memoria)
+        mult(parametros)
+    elif registradores['IR'] == 'JUMP+':
+        jump_plus(parametros)
 
     memoria.seek(0)
-    
-def inst_store(parametros, memoria):
+
+def jump_plus(parametros):
+    if registradores['AC'] >= 0:
+        registradores['PC'] = int(parametros[0], 16)
+        registradores['MAR'] = registradores['PC']
+
+def store(parametros, memoria):
+    memoria.seek(0)
     if len(parametros) == 1:
-        endereco_armazenamento = int(parametros[0], 16)
-        memoria.seek(endereco_armazenamento * TAM_LINHA + ENDERECO)
-        memoria.write(str(registradores['AC']).encode())
-    else:
-        endereco_armazenamento = int(parametros[1], 16)
-        memoria.seek(endereco_armazenamento * TAM_LINHA + ENDERECO)
-        registrador = parametros[0].upper()
+        memoria.seek(registradores['MAR'] * TAM_LINHA + ENDERECO)
+        memoria.write(registradores['MBR'].encode())
+    elif len(parametros) == 2:
+        memoria.seek(registradores['MAR'] * TAM_LINHA + ENDERECO)
 
-        memoria.write(str(registradores[registrador]).encode())
+        memoria.write(registradores['MBR'].encode())
 
-def inst_mov(parametros):
+def mov(parametros):
     if len(parametros) == 2:
         registrador_destino = parametros[0].upper()
         registrador_valor_origem = parametros[1].upper()
 
         registradores[registrador_destino] = registradores[registrador_valor_origem]
 
-def inst_add(parametros, memoria):
+def add(parametros):
     if len(parametros) == 1:
-        endereco_operando = int(parametros[0], 16)
-        memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
-
-        registradores['AC'] += int(memoria.readline().rstrip())
+        registradores['AC'] += int(registradores['MBR'])
     else:
-        if not parametros[1].startswith('0x'): # se não estiver em hex, se trata de um numero inteiro e não a posição da memoria (Ex: ADD B, 10)
-            valor = int(parametros[1])
+        registrador = parametros[0].upper()
+
+        registradores[registrador] += int(registradores['MBR'])
+
+def sub(parametros):
+    if len(parametros) == 1:
+        registradores['AC'] -= int(registradores['MBR'])
+    else:
+        registrador = parametros[0].upper()
+
+        registradores[registrador] -= int(registradores['MBR'])
+
+def mult(parametros):
+    if len(parametros) == 1:
+        registradores['MQ'] *= int(registradores['MBR'])
+    else:
+        registrador = parametros[0].upper()
+
+        registradores[registrador] *= int(registradores['MBR'])
+
+def div(parametros):
+    if len(parametros) == 1:
+        registradores['MQ'] = int(registradores['MBR'])
+    else:
+        registrador = parametros[0].upper()
+
+        registradores[registrador] = int(registradores[registrador] / int(registradores['MBR']))
+
+def load(parametros: list):
+    if len(parametros) == 1:
+        registradores['AC'] = int(registradores['MBR'])
+    else:
+        registrador = parametros[0].upper()
+
+        registradores[registrador] = int(registradores['MBR'])
+
+def verifica_hex(valor):
+    return valor.startswith('0x')
+
+def verifica_registrador(valor):
+    return valor.upper() in registradores
+
+def decodificacao_instrucao(parametros, memoria):
+    if len(parametros) == 2:
+        if verifica_registrador(parametros[0]) and verifica_registrador(parametros[1]):
+            registradores['MBR'] = str(registradores[parametros[1].upper()])
+
+        elif verifica_registrador(parametros[0]) and verifica_hex(parametros[1]) and registradores['IR'] == 'STORE':
+            registradores['MAR'] = int(parametros[1], 16)
+            registradores['MBR'] = str(registradores[parametros[0].upper()])
+
+        elif verifica_registrador(parametros[0]) and verifica_hex(parametros[1]):
+            registradores['MAR'] = int(parametros[1], 16)
+            memoria.seek(TAM_LINHA*registradores['MAR']+ENDERECO)
+            registradores['MBR'] = memoria.readline().decode().rstrip()
+
+        elif not verifica_hex(parametros[1]):
+            registradores['MBR'] = parametros[1]
+
+    elif len(parametros) == 1:
+        if verifica_registrador(parametros[0]):
+            registradores['MBR'] = str(registradores[parametros[0].upper()])
+
+        elif verifica_hex(parametros[0]) and registradores['IR'] == 'STORE':
+            registradores['MAR'] = int(parametros[0], 16)
+            registradores['MBR'] = str(registradores['AC'])
+
         else:
-            endereco_operando = int(parametros[1], 16)
-            memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
-            valor = int(memoria.readline().rstrip())
+            registradores['MAR'] = int(parametros[0], 16)
+            memoria.seek(TAM_LINHA*registradores['MAR'] + ENDERECO)
+            registradores['MBR'] = memoria.readline().decode().rstrip()
 
-        registrador = parametros[0].upper()
+def trata_enderecos(elementos_instrucao):
+    parametros = []
+    for i in range(1, len(elementos_instrucao)):
+        parametros.append(elementos_instrucao[i].rstrip(','))
+    return parametros
 
-        registradores[registrador] += valor
+def controle_fluxo_registradores():
+    while True:
+        imprime_registradores()
+        if inp := input('Pressione <ENTER> para continuar...\n') == '':
+            break
 
+def ciclo_busca(memoria):
+    offset_endereco = TAM_LINHA * registradores['MAR'] + ENDERECO
+    memoria.seek(0)
+    memoria.seek(offset_endereco)
+    instrucao = memoria.readline().decode().rstrip()
 
-def inst_sub(parametros, memoria):
-    if len(parametros) == 1:
-        endereco_operando = int(parametros[0], 16)
-        memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
+    registradores['MBR'] = instrucao
+    elementos_instrucao = registradores['MBR'].split()
+    registradores['IR'] = elementos_instrucao[0]
 
-        registradores['AC'] -= int(memoria.readline().rstrip())
-    else:
-        endereco_operando = int(parametros[1], 16)
-        memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
-        valor = int(memoria.readline().rstrip())
-        registrador = parametros[0].upper()
+    return instrucao
 
-        if not parametros[1].startswith('0x'): # se não estiver em hex, se trata de um numero inteiro e não a posição da memoria (Ex: ADD B, 10)
-            valor = int(parametros[1])
-
-        registradores[registrador] -= valor
-
-def inst_mult(parametros, memoria):
-    if len(parametros) == 1:
-        endereco_operando = int(parametros[0], 16)
-        memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
-
-        registradores['MQ'] = int(memoria.readline().rstrip())
-    else:
-        endereco_operando = int(parametros[1], 16)
-        memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
-
-        valor = int(memoria.readline().rstrip())
-        registrador = parametros[0].upper()
-
-        registradores[registrador] *= valor
-
-def inst_div(parametros, memoria):
-    if len(parametros) == 1:
-        endereco_operando = int(parametros[0], 16)
-        memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
-
-        registradores['MQ'] = int(memoria.readline().rstrip())
-    else:
-        endereco_operando = int(parametros[1], 16)
-        memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
-
-        valor = int(memoria.readline().rstrip())
-        registrador = parametros[0].upper()
-
-        registradores[registrador] = int(registradores[registrador] / valor)
-
-def inst_load(parametros: list, memoria):
-    if len(parametros) == 1:
-        endereco_operando = int(parametros[0], 16)
-        memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
-
-        registradores['AC'] = int(memoria.readline().rstrip())
-    else:
-        endereco_operando = int(parametros[1], 16)
-        memoria.seek(endereco_operando * TAM_LINHA + ENDERECO)
-
-        valor = int(memoria.readline().rstrip())
-        registrador = parametros[0].upper()
-
-        registradores[registrador] = valor
-
-def executa_instrucoes(memoria, num_instrucoes):
+def executa_instrucoes(memoria):
     registradores['MAR'] = registradores['PC']
-    for i in range(num_instrucoes):
-        offset_endereco = TAM_LINHA * registradores['MAR'] + ENDERECO
-        memoria.seek(0)
-        memoria.seek(offset_endereco)
-        registradores['MBR'] = memoria.readline().decode().rstrip()
 
-        elementos_instrucao = registradores['MBR'].split()
-        registradores['IR'] = elementos_instrucao[0]
-        parametros = []
-        for i in range(1, len(elementos_instrucao)):
-            parametros.append(elementos_instrucao[i].rstrip(','))
+    instrucao = ciclo_busca(memoria)
+
+    while registradores['IR'] != 'HALT':
+
+        print('Registradores antes da instrução:\n')
+        controle_fluxo_registradores()
+
+        parametros = trata_enderecos(registradores['MBR'].split())
         
-        verifica_opcode(parametros, memoria)
+        decodificacao_instrucao(parametros, memoria)
+        operacao_dados(parametros, memoria)
         
-        print(registradores['MBR'])
-        while True:
-            imprime_registradores()
-            if inp := input('Pressione <ENTER> para continuar...\n') == '':
-                break
-        
+        print(f"Registradores após a instrução ({instrucao}):\n")
+        controle_fluxo_registradores()
+        print('--------------------------------------------------\n')
+   
         registradores['PC']+=1
         registradores['MAR'] = registradores['PC']
 
-def inst_cmp(parametros):
-    if len(parametros) == 2:
-        pass
+        instrucao = ciclo_busca(memoria)
+
 
 def le_operacoes(arq, memoria):
     escreve_valores_memoria(arq, memoria)
@@ -218,9 +238,9 @@ def le_operacoes(arq, memoria):
     registradores['PC'] = int(arq.readline(), 16)
     memoria.seek(0)
 
-    num_instrucoes = escreve_instrucoes_memoria(arq, memoria)
+    escreve_instrucoes_memoria(arq, memoria)
     
-    executa_instrucoes(memoria, num_instrucoes)
+    executa_instrucoes(memoria)
 
 def main(nargs: int, args: list[str]) -> None:
     try:
